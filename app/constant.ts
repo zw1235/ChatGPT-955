d8a95ce3c91a592f244699baa46cd3b1698fc418
zw1235 <130537982+zw1235@users.noreply.github.com>
2023-05-07 05:06:37 +0800

    Update constant.ts

--- app/constant.ts
+++ app/constant.ts
@@ -1,8 +1,8 @@
 export const OWNER = "Yidadaa";
 export const REPO = "ChatGPT-Next-Web";
-export const REPO_URL = `https://github.com/${OWNER}/${REPO}`;
-export const ISSUE_URL = `https://github.com/${OWNER}/${REPO}/issues`;
-export const UPDATE_URL = `${REPO_URL}#keep-updated`;
+export const REPO_URL = `https://wxaurl.cn/IKidMEo28nk`;
+export const ISSUE_URL = `https://wxaurl.cn/IKidMEo28nk/issues`;
+export const UPDATE_URL = `https://public.zsxq.com/groups/51112181228454`;
 export const FETCH_COMMIT_URL = `https://api.github.com/repos/${OWNER}/${REPO}/commits?per_page=1`;
 export const FETCH_TAG_URL = `https://api.github.com/repos/${OWNER}/${REPO}/tags?per_page=1`;
 export const RUNTIME_CONFIG_DOM = "danger-runtime-config";
